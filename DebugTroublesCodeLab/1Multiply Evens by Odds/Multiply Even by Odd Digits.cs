23bcde61fb10b62ef655bd5aa612f4e60a071613
todorkolev81 <todor.kolev81@gmail.com>
2017-02-05 15:16:22 +0200

    add

---
+++ DebugTroublesCodeLab/1Multiply Evens by Odds/Multiply Even by Odd Digits.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numInput = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetMultipleOfEvensAndOdds(numInput));
+        }
+
+        private static int GetMultipleOfEvensAndOdds(int num)
+        {
+            int sumEvens = GetSumOfEvenDigits(num);
+            int sumOdds = GetSumOfOddDigits(num);
+            return sumEvens * sumOdds;
+        }
+
+        private static int GetSumOfOddDigits(int NumOddDigits)
+        {
+            int sum = 0;
+            NumOddDigits = Math.Abs(NumOddDigits);
+            while (NumOddDigits > 0)
+            {
+                int lastDigit = NumOddDigits % 10;
+                if (lastDigit % 2 != 0)
+                {
+                    sum += lastDigit;
+                }
+                NumOddDigits /= 10;
+            }
+            return sum;
+        }
+        private static int GetSumOfEvenDigits(int NumEvenDigits)
+        {
+            int sum = 0;
+            NumEvenDigits = Math.Abs(NumEvenDigits);
+            while (NumEvenDigits > 0)
+            {
+                int lastDigit = NumEvenDigits % 10;
+                if (lastDigit % 2 == 0)
+                {
+                    sum += lastDigit;
+                }
+                NumEvenDigits /= 10;
+            }
+            return sum;
+        }
+    }
+}
---
+++ DebugTroublesCodeLab/3Debugging Exercise Price Change Alert/Debugging Exercise Price Change Alert.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Debugging_Exercise_Price_Change_Alert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double procentInput = double.Parse(Console.ReadLine());
+            double lastNum = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                double divieded = ResultFromDevidingTheNumbers(lastNum, number);
+                bool isSignificantDifference = Difference(divieded, procentInput);
+                string message = GetTheFinalText(number, lastNum, divieded, isSignificantDifference);
+                Console.WriteLine(message);
+
+                lastNum = number;
+            }
+        }
+
+        private static string GetTheFinalText(double currNum, double currLastNum, double difference, bool trueOrFalse)
+
+        {
+            string printText = "";
+            if (difference == 0)
+            {
+                printText = string.Format("NO CHANGE: {0}", currNum);
+            }
+            else if (!trueOrFalse)
+            {
+                printText = string.Format("MINOR CHANGE: {0} to {1} ({2:F2}%)", currLastNum, currNum, difference * 100);
+            }
+            else if (trueOrFalse && (difference > 0))
+            {
+                printText = string.Format("PRICE UP: {0} to {1} ({2:F2}%)", currLastNum, currNum, difference*100);
+            }
+            else if (trueOrFalse && (difference < 0))
+                printText = string.Format("PRICE DOWN: {0} to {1} ({2:F2}%)", currLastNum, currNum, difference*100);
+            return printText;
+        }
+        private static bool Difference(double devidedTo, double difference)
+        {
+            if (Math.Abs(devidedTo) >= difference)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static double ResultFromDevidingTheNumbers(double currCurr, double currLast)
+        {
+            double resultFromDeviding = (currLast - currCurr) / currCurr;
+            return resultFromDeviding;
+
+        }
+    }
+}
