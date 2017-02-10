f47518883b059030b1fe24cfe3d85a28491875bd
todorkolev81 <todor.kolev81@gmail.com>
2017-02-10 22:46:49 +0200

    add 04

--- Methods Defining and Calling Methods/7Greater of Two Values/Greater of Two Values.cs
+++ Methods Defining and Calling Methods/7Greater of Two Values/Greater of Two Values.cs
@@ -10,46 +10,57 @@ namespace _7Greater_of_Two_Values
     {
         static void Main(string[] args)
         {
-            string type = Console.ReadLine();
-            var first = int.Parse(Console.ReadLine());
-            var second = int.Parse(Console.ReadLine());
-
-            var result = GetMax(first, second);
-
-            Console.WriteLine(result);
+            var type = Console.ReadLine();
+            
+            switch (type)
+            {
+                case "int":
+                    {
+                        var value1 = int.Parse(Console.ReadLine());
+                        var value2 = int.Parse(Console.ReadLine());
+                        var bigger = GetMax(value1, value2);
+                        Console.WriteLine(bigger);
+                        break;
+                    }
+                case "char":
+                    {
+                        var value1 = char.Parse(Console.ReadLine());
+                        var value2 = char.Parse(Console.ReadLine());
+                        var bigger = GetMax(value1, value2);
+                        Console.WriteLine(bigger);
+                        break;
+                    }
+                case "string":
+                    {
+                        string value1 = Console.ReadLine();
+                        string value2 = Console.ReadLine();
+                        string bigger = GetMax(value1, value2);
+                        Console.WriteLine(bigger);
+                        break;
+                    }
+            }
         }
-        static int GetMax(int first, int second)
+        static int GetMax(int num1, int num2)
         {
-            if (first >= second)
+            if (num1 > num2)
             {
-                return first;
+                return num1;
             }
-            else
-            {
-                return second;
-            }
+            return num2;
         }
-        static char GetMax(char first, char second)
+        static char GetMax(char char1, char char2)
         {
-            if (first >= second)
-            {
-                return first;
-            }
-            else
-            {
-                return second;
-            }
+            int char1Int = char1;
+            int char2Int = char2;
+            return (char)GetMax(char1Int, char2Int);
         }
-        static string GetMax(string first, string second)
+        static string GetMax(string string1, string string2)
         {
-            if (first.CompareTo(second) >= 0)
+            if (string1.CompareTo(string2) >= 0)
             {
-                return first;
+                return string1;
             }
-            else
-            {
-                return second;
-            }
+            return string2;
         }
     }
 }
