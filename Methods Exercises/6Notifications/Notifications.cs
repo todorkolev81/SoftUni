d57e378d29328d0f1fd75a2442d71d468401929a
todorkolev81 <todor.kolev81@gmail.com>
2017-02-11 17:44:55 +0200

    04 exer

---
+++ Methods Exercises/6Notifications/Notifications.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6Notifications
+{
+    class Notifications
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var value1 = Console.ReadLine();
+                switch (value1)
+                {
+                    case "success":
+                        {
+                            var input1 = Console.ReadLine();
+                            var input2 = Console.ReadLine();
+                            ShowSuccess(input1, input2);
+                            break;
+                        }
+                    case "error":
+                        {
+                            var input1 = Console.ReadLine();
+                            var input2 = int.Parse(Console.ReadLine());
+                            ShowError(input1, input2);
+                            break;
+                        }
+                }
+            }
+        }
+
+        static void ShowSuccess(string operation, string message)
+        {
+            Console.WriteLine($"Successfully executed {operation}.");
+            Console.WriteLine("==============================");
+            Console.WriteLine($"Message: {message}.");
+        }
+
+        static void ShowError(string operation, int code)
+        {
+            Console.WriteLine($"Error: Failed to execute {operation}.");
+            Console.WriteLine("==============================");
+            Console.WriteLine($"Error Code: {code}.");
+            if (code < 0)
+            {
+                Console.WriteLine($"Reason: Internal System Failure.");
+            }
+            else
+            {
+                Console.WriteLine($"Reason: Invalid Client Data.");
+
+            }
+
+        }
+
+
+    }
+}
+
---
+++ Methods Exercises/7Numbers to Words/NumbersToWords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7Numbers_to_Words
+{
+    class NumbersToWords
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+        }
+    }
+}
