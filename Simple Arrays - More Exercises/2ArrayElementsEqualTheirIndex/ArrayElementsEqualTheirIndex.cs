0710d54b863a6b6a6b44fbc14e7f6e5b421b17b5
todorkolev81 <todor.kolev81@gmail.com>
2017-02-12 14:02:11 +0200

    more to 06

---
+++ Simple Arrays - More Exercises/2ArrayElementsEqualTheirIndex/ArrayElementsEqualTheirIndex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ArrayElementsEqualTheirIndex
+{
+    class ArrayElementsEqualTheirIndex
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var result = "";
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == i)
+                {
+                    result = i + " ";
+                }
+            }
+            Console.WriteLine(result+);
+        }
+    }
+}
--- Simple Arrays - More Exercises/3Phonebook/Phonebook.cs
+++ Simple Arrays - More Exercises/3Phonebook/Phonebook.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace _3Phonebook
 {
-    class Program
+    class Phonebook
     {
         static void Main(string[] args)
         {
+            var numbers = Console.ReadLine().Split(' ');
+            var names = Console.ReadLine().Split(' ');
+            var inputNames = Console.ReadLine();
+
+            while (inputNames != "done")
+            {
+                PrintNumbersAndNames(numbers, names, inputNames);
+
+                inputNames = Console.ReadLine();
+            }
+        }
+
+        private static void PrintNumbersAndNames(string[] numbers, string[] names, string inputNames)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (inputNames == names[i])
+                {
+                    Console.WriteLine("{0} -> {1}", inputNames, numbers[i]);
+                    break;
+                }
+            }
         }
     }
 }
+
