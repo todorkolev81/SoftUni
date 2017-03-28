2fbcd0c7330bb7bbc4602be54ec9299a432b09e1
todorkolev81 <todor.kolev81@gmail.com>
2017-03-28 10:34:29 +0300

    13 update

--- Files, Dirs and Exceptions - Exercises/2HTMLContents/HTMLContents.cs
+++ Files, Dirs and Exceptions - Exercises/2HTMLContents/HTMLContents.cs
@@ -11,19 +11,75 @@ namespace _2HTMLContents
     {
         public static void Main(string[] args)
         {
-            var lines = File.ReadAllLines("Input.txt");
+            var lines = Console.ReadLine();
+            //var lines = File.ReadAllLines("Input.txt");
             var title = string.Empty;
-            var bodyPArts = new List<string>();
+            var bodyParts = new List<string>();
 
-            foreach (var line in lines)
+            while (lines != "exit")
             {
-                if (line=="exit")
-                {
-                    break;
-                }
-                var lineParts = line.Split(' ');
+                //foreach (var line in lines)
+                //{
+                //var lineParts = line.Split(' ');
+                var lineParts = lines.Split(' ');
 
+                var tag = lineParts[0];
+                var tagContent = lineParts[1];
+
+                if (tag == "title")
+                {
+                    title = tagContent;
+                }
+                else
+                {
+                    bodyParts.Add($"\t<{tag}>{tagContent}</{tag}>");
+                }
+                //}
+                lines = Console.ReadLine();
             }
+            //foreach (var line in lines)
+            //{
+            //    if (line == "exit")
+            //    {
+            //        break;
+            //    }
+            //    var lineParts = line.Split(' ');
+
+            //    var tag = lineParts[0];
+            //    var tagContent = lineParts[1];
+
+            //    if (tag == "title")
+            //    {
+            //        title = tagContent;
+            //    }
+            //    else
+            //    {
+            //        bodyParts.Add($"\t<{tag}>{tagContent}</{tag}>");
+            //    }
+            //}
+
+            var result = new StringBuilder();
+            result.AppendLine("<!DOCTYPE html>");
+            result.AppendLine("<html>");
+            result.AppendLine("<head>");
+
+
+            if (title != string.Empty)
+            {
+                result.AppendLine($"\t<title>{title}</title>");
+            }
+
+            result.AppendLine("</head>");
+            result.AppendLine("<body>");
+
+            if (bodyParts.Any())
+            {
+                result.AppendLine(string.Join(Environment.NewLine, bodyParts));
+            }
+            result.AppendLine("</body>");
+            result.AppendLine("</html>");
+
+            File.WriteAllText("index.html", result.ToString().Trim());
         }
     }
 }
