e63fd12369c3ad4df1e09d75ac0a0aef6cd944f6
todorkolev81 <todor.kolev81@gmail.com>
2017-04-17 23:20:51 +0300

    RegEx - Exercises

---
+++ Regular Expressions (RegEx) - Exercises/1Cards/Cards.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _1Cards
+{
+    class Cards
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"([2-9JQKA]{1}[SHDC])|(\d[0-9JQKA])[SHDC]";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matchedCard = regex.Matches(input);
+
+            var list = new List<string>();
+
+            foreach (Match card in matchedCard)
+            {
+                var check = 0;
+
+                if (int.TryParse(card.Groups[2].Value, out check))
+                {
+                    if (check < 2 || check > 10)
+                    {
+                        continue;
+                    }
+                }
+                list.Add(card.Value);
+            }
+
+            Console.WriteLine(string.Join(", ", list));
+
+        }
+    }
+}
---
+++ Regular Expressions (RegEx) - Exercises/2FishStatistics/FishStatistics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _2FishStatistics
+{
+    class FishStatistics
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"(>*<*\(*'>?)|(>*<*\(*->?)|(>*<*\(*x>?)";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection validFish = regex.Matches(input);
+
+            var list = new List<string>();
+
+            foreach (Match fish in validFish)
+            {
+                //var check = 0;
+
+                //if (int.TryParse(card.Groups[2].Value, out check))
+                //{
+                //    if (check < 2 || check > 10)
+                //    {
+                //        continue;
+                //    }
+                //}
+                list.Add(fish.Value);
+            }
+        }
+    }
+}
